0f59f109aa05e3f912e367ef60672afaee163f76
Viraj Bhanushali <104625176+Azreal18@users.noreply.github.com>
2026-08-30 20:25:15 +0530

    update

--- script.js
+++ script.js
@@ -167,7 +167,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Replace this with the Web App URL you get after deploying the script in
 // google-apps-script/Code.gs.
-const VISITOR_LOG_ENDPOINT = 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE';
+const VISITOR_LOG_ENDPOINT = 'https://script.google.com/macros/s/AKfycbxQDG3ThcQir8f88ESADeGaU4kK_bjxhEIfnlHL3qNq8t-allNChsyvrZsPVANUsHFh3w/exec';
 
 (function logVisit() {
   // Skip entirely if the endpoint hasn't been configured yet, or if the
